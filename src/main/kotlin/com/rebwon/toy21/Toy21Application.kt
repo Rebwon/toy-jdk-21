ef72f06ff6d6616b58979184f5f921a630e5cf6b
solomon.maeng <solomon.maeng@dramancompany.com>
2023-12-05 19:38:12 +0900

    Initial Commit

---
+++ src/main/kotlin/com/rebwon/toy21/Toy21Application.kt
@@ -0,0 +1,11 @@
+package com.rebwon.toy21
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Toy21Application
+
+fun main(args: Array<String>) {
+    runApplication<Toy21Application>(*args)
+}
---
+++ src/test/kotlin/com/rebwon/toy21/Toy21ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rebwon.toy21
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Toy21ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
